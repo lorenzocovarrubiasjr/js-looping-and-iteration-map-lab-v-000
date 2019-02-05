05aa3749a2308e1c504a3935fd50ff92b087c7ee
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-02-05 02:30:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array) {
+  
+  let lowerCasedArray = array.map(function(x) {
+    return x.toLowerCase();
+  })
+  return lowerCasedArray;
+} 
+
+function nameToAttributes(array) {
+  array.map(x )
+}
