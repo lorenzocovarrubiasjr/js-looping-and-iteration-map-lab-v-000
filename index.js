779fe2f0cd0bac8482f1b294be419685f4e02a25
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-02-05 03:03:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array) {
+
+  let lowerCasedArray = array.map(function(x) {
+    return x.toLowerCase();
+  })
+  return lowerCasedArray;
+}
+
+function nameToAttributes(array) {
+  let objectifiedNames = [];
+  array.map(function(x) {
+    let a = x.split(" ")[0]
+    let b = x.split(" ")[1]
+    objectifiedNames.push({firstName: a, lastName: b});
+  })
+
+  return objectifiedNames;
+}
+
+function attributesToPhrase(array) {
+  let printOut = array.map(function(x){
+    return `${x.name} is from ${x.hometown}`
+  });
+  return printOut;
+}
